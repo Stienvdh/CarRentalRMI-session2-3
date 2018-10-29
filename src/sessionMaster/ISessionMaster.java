0a8624c23e07ad0258418bb4dfef428ed4c879c0
Stien <stien.vanderhallen@hotmail.com>
2018-10-29 12:11:44 +0100

    class SessionMaster

--- src/sessionMaster/ISessionMaster.java
+++ src/sessionMaster/ISessionMaster.java
@@ -1,5 +1,7 @@
 package sessionMaster;
 
+import session.IManagerSession;
+import session.IReservationSession;
 import session.Session;
 
 import java.rmi.Remote;
@@ -9,5 +11,10 @@ import java.util.List;
 public interface ISessionMaster extends Remote {
 
     List<Session> getActiveSessions() throws RemoteException;
+    IReservationSession addReservationSession(String sessionid) throws RemoteException;
+    IManagerSession addManagerSession(String sessionid) throws RemoteException;
+    IReservationSession getReservationSession(String sessionid) throws RemoteException;
+    IManagerSession getManagerSession(String sessionid) throws RemoteException;
+    void terminateSession(String sessionid) throws RemoteException, IllegalArgumentException;
 
 }
